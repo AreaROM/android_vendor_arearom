f921dd0b751896cd1d85a3d2f3912f488c866606
Motorhead1991 <motor-head-1991@hotmail.com>
2013-08-18 19:32:09 -0700

    Initial Commit

---
+++ products/common.mk
@@ -0,0 +1,18 @@
+# Tiny toolbox
+TINY_TOOLBOX:=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false
+
+# T-Mobile theme engine
+include vendor/cyanogen/products/themes_common.mk
+
+# Required, keyboard
+PRODUCT_PACKAGES += LatinIME
+
