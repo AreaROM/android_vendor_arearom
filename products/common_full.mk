17c158b01b60f966f344bb12c799245d78cca82a
Anthony Cox <motorhead1991@hotmail.com>
2015-06-09 20:47:37 -0600

    Enable AreaROM overlays

--- products/common_full.mk
+++ products/common_full.mk
@@ -3,3 +3,6 @@ $(call inherit-product, vendor/arearom/products/common_full_no_themes.mk)
 
 # Theme packages
 include vendor/arearom/products/themes.mk
+
+# AreaROM overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/arearom/overlay/common
