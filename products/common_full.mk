33611260f029c3b97c1ad0527b00ab886eb30110
Anthony Cox <motorhead1991@hotmail.com>
2015-08-09 22:40:44 -0600

    Remove "theme" files, we're switching to RRO

--- products/common_full.mk
+++ products/common_full.mk
@@ -1,8 +1,2 @@
-# Inherit common CM stuff
-$(call inherit-product, vendor/arearom/products/common_full_no_themes.mk)
-
-# Theme packages
-include vendor/arearom/products/themes.mk
-
 # AreaROM overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/arearom/overlay/common
